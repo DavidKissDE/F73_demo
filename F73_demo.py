afafcbda55d51804c124708acde6d93ba31fd886
David Kiss <davidkiss83@gmail.com>
2023-11-05 00:44:36 +0100

    small edits

--- F73_demo.py
+++
@@ -1,7 +0,0 @@
-# %
-import pandas as pd
-df = pd.read_csv("data.csv")
-df.columns # 'country', 'year', 'population', 'continent', 'life_exp', 'gdp_cap'
-# %
-
-df.loc[:,["country", "life_exp", "gdp_cap"]]
